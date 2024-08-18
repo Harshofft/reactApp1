09ee3b85e27a4c9888cb87e473d2a7adf4065400
Harshofft <hdharsh122@gmail.com>
2024-08-18 08:06:26 +0530

    xyz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello World</h1>);
+// function greet(names) {
+//     console.log(names);
+//     return (<h1>hey{names}</h1>)   
+// }
+const Hello =(probs)=>{
+    console.log(probs);
+    return (<h1>hello</h1>)
+}
+
+
+root.render(<>
+<Hello names="harry" city="newyork"/>
+<h1>Hello World </h1>
+{/* <greet names="harry"/>
+<greet names="harry"/>   */}
+</>);
